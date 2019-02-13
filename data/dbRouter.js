52adc43baf3c0d0c7991ca496765ea8e448487f9
Jacob Bryan <jtb36688@gmail.com>
2019-02-13 02:08:36 -0500

    Delete and Put done

--- data/dbRouter.js
+++ data/dbRouter.js
@@ -1,101 +1,98 @@
-const express = require('express');
-const db = require('./db.js');
+const express = require("express");
+const db = require("./db.js");
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body;
+  const addition = { title, contents };
 
-
-// If the request body is missing the title or contents property:
-// cancel the request.
-// respond with HTTP status code 400 (Bad Request).
-// return the following JSON response: { errorMessage: "Please provide title and contents for the post." }.
-
-// If the information about the post is valid:
-// save the new post the the database.
-// return HTTP status code 201 (Created).
-// return the newly created post.
-
-// If there's an error while saving the post:
-// cancel the request.
-// respond with HTTP status code 500 (Server Error).
-// return the following JSON object: { error: "There was an error while saving the post to the database" }.
-
-
-router.post('/', async (req, res) => {
-const { title, contents } = req.body
-const addition = { title, contents }
-
-if (!title || !contents ) {
-    return res.status(400).json( { errorMessage: "Please provide title and contents for the post." } )
-}
-try {
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  try {
     const posts = await db.insert(addition);
     res.status(201).json(posts);
-} catch (error) {
-    res.status(500).json({ error: "There was an error while saving the post to the database" })
-}
-})
-
-// When the client makes a GET request to /api/posts:
-
-//     If there's an error in retrieving the posts from the database:
-//         cancel the request.
-//         respond with HTTP status code 500.
-//         return the following JSON object: { error: "The posts information could not be retrieved." }.
-
-// When the client makes a GET request to /api/posts/:id:
-
-//     If the post with the specified id is not found:
-//         return HTTP status code 404 (Not Found).
-//         return the following JSON object: { message: "The post with the specified ID does not exist." }.
-
-//     If there's an error in retrieving the post from the database:
-//         cancel the request.
-//         respond with HTTP status code 500.
-//         return the following JSON object: { error: "The post information could not be retrieved." }.
-
-
-router.get('/', async (req, res) => {
-try {
-const posts = await db.find(req.query);
-res.status(200).json(posts);
-} catch (error) {
-res.status(500).json({
-    error: "The posts information could not be retrieved."
-})
-}
-})
-
-router.get('/:id', async (req, res) => {
-const { id } = req.params
-try {
-const found = await db.findById(id)
-    if (found) {
-        res.status(200).json(found)
-    }
-    else {
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    }
-} catch (error) {
+  } catch (error) {
     res.status(500).json({
-        error: "The post information could not be retrieved." 
-    })
-}
-})
+      error: "There was an error while saving the post to the database"
+    });
+  }
+});
 
-// router.delete('/:id', async (req, res) => {
-// try {
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db.find(req.query);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({
+      error: "The posts information could not be retrieved."
+    });
+  }
+});
 
-// } catch (error) {
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const found = await db.findById(id);
+    if (found) {
+      res.status(200).json(found);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "The post information could not be retrieved."
+    });
+  }
+});
 
-// }
-// })
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await db.remove(id);
+    if (deleted) {
+      db.findById(id).then(get => {
+        res.status(204).json(get);
+      });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "The post could not be removed" });
+  }
+});
 
-// router.put('/:id', async (req, res) => {
-// try {
-
-// } catch (error) {
-
-// }
-// })
+router.put("/:id", async (req, res) => {
+  const { title, contents } = req.body;
+  const { id } = req.params;
+  const changes = { title, contents };
+  if (!title || !contents) {
+    return res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    });
+  }
+  try {
+    const update = await db.update(id, changes);
+    if (update) {
+      db.find().then(get => {
+        res.status(200).json(get);
+      });
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist."
+      });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "The post information could not be modified." });
+  }
+});
 
 module.exports = router;
