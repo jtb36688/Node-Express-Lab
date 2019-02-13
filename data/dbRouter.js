d7cdcffe0d9574318f9705d91280d2e3d234e3d5
Jacob Bryan <jtb36688@gmail.com>
2019-02-12 23:31:58 -0500

    Boilerplated routes

---
+++ data/dbRouter.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const db = require('./db.js');
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+try {
+    
+} catch (error) {
+
+}
+})
+
+router.get('/', async (req, res) => {
+try {
+
+} catch (error) {
+
+}
+})
+
+router.get('/:id', async (req, res) => {
+try {
+
+} catch (error) {
+
+}
+})
+
+router.delete('/:id', async (req, res) => {
+try {
+
+} catch (error) {
+
+}
+})
+
+router.put('/:id', async (req, res) => {
+try {
+
+} catch (error) {
+
+}
+})
+
+module.exports = router;
